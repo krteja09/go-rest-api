9f45e750f0c4fedb55a3936e0d0bf944ce5c7f93
Raviteja Karumanchi <lakshmianusharemala@Lakshmis-MacBook-Pro.local>
2021-06-04 15:10:55 -0500

    Initial Commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+
+//App -struct which contains things like pointers to database connections
+type App struct {
+
+}
+
+func (app *App)Run() error {
+	fmt.Println("Setting up Error")
+	return nil
+}
 func main()  {
 	fmt.Println("Go Rest API ")
+	app := App{}
+	if err := app.Run(); err !=nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
